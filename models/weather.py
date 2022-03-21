b66bca13a49c40474a58106a6d7da7eb7c47c35c
Bohdan Pokhidnia <bogdanpoh@icloud.com>
2022-03-21 20:17:57 +0200

    added showing temperature for weather

--- models/weather.py
+++ models/weather.py
@@ -4,27 +4,35 @@ from helpers import constants
 
 class Weather(BaseModel):
     def __init__(self, data):
-        location = data['location']
-        current = data['current']
-        condition = current['condition']
-        icon = str(condition['icon'])\
+        location = data["location"]
+        current = data["current"]
+        condition = current["condition"]
+        icon = str(condition["icon"])\
             .replace("//", "") \
             .replace("64x64", "128x128")
 
-        self.city_name = location['name']
-        self.date_time = location['localtime']
-        self.last_update = current['last_updated']
-        self.is_day = int(current['is_day']) == 1
-        self.text = condition['text']
+        self.country = location["country"]
+        self.city_name = location["name"]
+        self.region = location["region"]
+        self.date_time = location["localtime"]
+        self.last_update = current["last_updated"]
+        self.is_day = int(current["is_day"]) == 1
+        self.temperature = round(current["temp_c"])
+        self.text = condition["text"]
         self.icon_url = f"http://{icon}"
-        self.cloud = f"{current['cloud']}%"
+        self.cloud = current["cloud"]
+
+        from pprint import pprint
+
+        pprint(data)
 
     def info(self):
         return f"""
-Місто: {self.city_name}
+{self.country}, {self.city_name}, {self.region}
 Дата: {self.date_time}
 Останнє оновлення: {self.last_update}
 
+Температура: {self.temperature}°
 {self.text} {constants.day_emoji if self.is_day else constants.night_emoji}
-Облачність: {self.cloud} {constants.cloud_emoji}
+Хмарність: {self.cloud}% {constants.cloud_emoji}
         """
